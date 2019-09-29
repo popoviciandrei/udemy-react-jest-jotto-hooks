5d4de59857757b34efb5c79a57312c4c0ed8c4a8
Andrei Popovici <me@andreipopovici.co.uk>
2019-09-29 22:22:34 +0100

    Added language context to GuessedWords template

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,65 +1,67 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const GuessedWords = (props) => {
-    let contents;
-    if (props.guessedWords.length === 0) {
-        contents = (
-            <span data-test="guess-instructions">
-                Try to guess the secret word!
-            </span>
-        );
-    } else {
-        const guessedWordRows = props.guessedWords.map((word, index) => (
-            <tr data-test="guessed-word" key={index}>
-                <td>{index + 1}</td>
-                <td>{word.guessedWord}</td>
-                <td>{word.letterMatchCount}</td>
-            </tr>
-        ));
+import languageContext from "./contexts/languageContext";
+import stringModule from "./helpers/strings";
 
-        const guessedWordCounter = (
+const GuessedWords = props => {
+  const language = React.useContext(languageContext);
+
+  let contents;
+
+  const getString = key => {
+    return stringModule.getStringByLanguage(language, key);
+  };
+
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">{getString("guessPrompt")}</span>
+    );
+  } else {
+    const guessedWordRows = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{index + 1}</td>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+
+    const guessedWordCounter = (
+      <tr>
+        <td data-test="total-guesses">
+          Total guesses: {props.guessedWords.length}
+        </td>
+      </tr>
+    );
+
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table className="table table-sm table-light table-bordered">
+          <thead className="thead-light">
             <tr>
-                <td data-test='total-guesses'>Total guesses: {props.guessedWords.length}</td>
-            </tr >);
+              <th>#</th>
+              <th>{getString("guessColumnHeader")}</th>
+              <th>{getString("matchingLettersColumnHeader")}</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordRows}</tbody>
+          <tfoot>{guessedWordCounter}</tfoot>
+        </table>
+      </div>
+    );
+  }
 
-        contents = (
-            <div data-test="guessed-words">
-                <h3>Guessed Words</h3>
-                <table className="table table-sm table-light table-bordered">
-                    <thead className="thead-light">
-                        <tr>
-                            <th>#</th>
-                            <th>Guess</th>
-                            <th>Mathcing letters</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {guessedWordRows}
-                    </tbody>
-                    <tfoot>
-                        {guessedWordCounter}
-                    </tfoot>
-                </table>
-            </div>
-        );
-    }
-
-    return (
-        <div data-test="component-guessed-words">
-            {contents}
-        </div>
-    )
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessedWords.propTypes = {
-    guessedWords: PropTypes.arrayOf(
-        PropTypes.shape({
-            guessedWord: PropTypes.string.isRequired,
-            letterMatchCount: PropTypes.number.isRequired
-        })
-    ).isRequired
-
-}
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
 
 export default GuessedWords;
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,80 +1,95 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { checkProps, findByTestAttr } from '../test/testUtils';
-import GuessedWords from './GuessedWords';
-import TestRunner from 'jest-runner';
+import React from "react";
+import { shallow } from "enzyme";
+import { checkProps, findByTestAttr } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+import TestRunner from "jest-runner";
 
 const defaultProps = {
-    guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
-}
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }]
+};
 
 const setup = (props = {}) => {
-    const setupProps = { ...defaultProps, ...props };
-    return shallow(<GuessedWords {...setupProps} />);
-}
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
 
-test('does not throw warning with expected props', () => {
-    checkProps(GuessedWords, defaultProps);
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
 });
 
-describe('if there are no words guessed', () => {
-    let wrapper;
-    // define wrapper that will be used by each sub test
-    beforeEach(() => {
-        wrapper = setup({ guessedWords: [] });
-    });
+describe("if there are no words guessed", () => {
+  let wrapper;
+  // define wrapper that will be used by each sub test
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
 
-    test('renders without errors', () => {
-        const component = findByTestAttr(wrapper, 'component-guessed-words');
-        expect(component.length).toBe(1);
-    })
+  test("renders without errors", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
 
-    test('renders instructions to guess a word', () => {
-        const instructions = findByTestAttr(wrapper, 'guess-instructions');
-        expect(instructions.text().length).not.toBe(0);
-    })
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
 
-    test('don`t render total no of guesses section', () => {
-        const totalGuessedWords = findByTestAttr(wrapper, 'total-guesses');
-        expect(totalGuessedWords.length).toBe(0);
-    });
-
+  test("don`t render total no of guesses section", () => {
+    const totalGuessedWords = findByTestAttr(wrapper, "total-guesses");
+    expect(totalGuessedWords.length).toBe(0);
+  });
 });
 
-describe('if there are words guessed', () => {
-    let wrapper;
-    const guessedWords = [
-        { guessedWord: 'train', letterMatchCount: 3 },
-        { guessedWord: 'agile', letterMatchCount: 1 },
-        { guessedWord: 'party', letterMatchCount: 5 },
-    ];
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 }
+  ];
 
-    beforeEach(() => {
-        wrapper = setup({ guessedWords });
-    });
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
 
-    test('renders without error', () => {
-        const component = findByTestAttr(wrapper, 'component-guessed-words');
-        expect(component.length).toBe(1);
-    });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
 
-    test('renders "guess words" section', () => {
-        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
-        expect(guessedWordsNode.length).toBe(1);
-    });
+  test('renders "guess words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
 
-    test('renders the corrent number of guessed words', () => {
-        const guessedWords = findByTestAttr(wrapper, 'guessed-word');
-        expect(guessedWords.length).toBe(guessedWords.length);
-    });
+  test("renders the corrent number of guessed words", () => {
+    const guessedWords = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWords.length).toBe(guessedWords.length);
+  });
 
-    test('renders total no of guesses section', () => {
-        const totalGuessedWords = findByTestAttr(wrapper, 'total-guesses');
+  test("renders total no of guesses section", () => {
+    const totalGuessedWords = findByTestAttr(wrapper, "total-guesses");
 
-        // element should be on the pate
-        expect(totalGuessedWords.length).toBe(1);
+    // element should be on the pate
+    expect(totalGuessedWords.length).toBe(1);
 
-        // element should containt the no of tries
-        expect(totalGuessedWords.text()).toContain(guessedWords.length);
-    })
+    // element should containt the no of tries
+    expect(totalGuessedWords.text()).toContain(guessedWords.length);
+  });
+});
+
+describe("languagePicker", () => {
+  test("correctly renders gues instructions string in english by default", () => {
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(guessInstructions.text()).toBe("Try to guess the secret word!");
+  });
+  test("correctly renders gues instructions string in emoji", () => {
+    const mockUseContext = jest.fn().mockReturnValue("emoji");
+    React.useContext = mockUseContext;
+
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(guessInstructions.text()).toBe("🤔🤫🔤");
+  });
 });
